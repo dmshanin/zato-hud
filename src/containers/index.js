14ac73da39feedcb607a2a14a489208a77835b06
Dmitriy Shanin <shanin_dv@shopandshow.ru>
2019-11-23 01:22:23 +0300

    поправил сборку и ассеты на главной

--- src/containers/index.js
+++
@@ -1,7 +0,0 @@
-import _Preloader from "./Preloader";
-import _PageList from "./PageList";
-import _Home from "./Home";
-
-export const Preloader = _Preloader;
-export const PageList = _PageList;
-export const Home = _Home;

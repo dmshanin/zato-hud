80f4899fa40bcc5d02d68d9aaa18a597e7aae00c
Dmitriy Shanin <shanin_dv@shopandshow.ru>
2019-11-24 16:28:23 +0300

    доработал кнопку меню, само меню с сылками, компонент кнопок, громкость

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-console.log('Hello!');
+// Menu
+(() => {
+  const navTogglerEl = document.querySelector('.nav__toggler .btn');
+  const navOverlayEl = document.querySelectorAll('.nav__content__overlay');
+  const navContentEl = document.querySelector('.nav__content');
+
+  navTogglerEl.addEventListener('click', () => {
+    navContentEl.classList.add('in');
+  });
+
+  navOverlayEl.forEach(item => {
+    item.addEventListener('click', () => {
+      navContentEl.classList.remove('in');
+    });
+  });
+})();
